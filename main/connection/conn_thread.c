3b2a54f1bbaf2153cbf0620057c6fb39c97c19a6
Peppe289 <gsperanza204@protonmail.com>
2025-12-31 11:07:36 +0100

    conn_thread.c: Initialize NVS.
    * required for BT
    
    Signed-off-by: Peppe289 <gsperanza204@protonmail.com>

--- main/connection/conn_thread.c
+++ main/connection/conn_thread.c
@@ -56,12 +56,12 @@ void connection_listener_start(void) {
   pthread_create(&tconfig, NULL, manageConnection, NULL);
 
   // Initialize NVS.
-  // ret = nvs_flash_init();
-  // if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-  //    ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-  //  ESP_ERROR_CHECK(nvs_flash_erase());
-  //  ret = nvs_flash_init();
-  // }
-  // ESP_ERROR_CHECK(ret);
+  ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+     ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+   ESP_ERROR_CHECK(nvs_flash_erase());
+   ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
   pthread_join(tconfig, NULL);
 }
